a018736a7b4c2ed3c25257bdbbaa6dc4ab53040f
unknown <danielyogatama.dy@gmail.com>
2021-11-10 17:09:29 +0700

    First commit

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+ 
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Yogatama",
+        "email" => "test@gmail.com",
+        "img" => "boredape7914.png"
+    ]);
+});
+ 
+Route::get('/blog', function () {
+    return view('posts');
+});
+ 
